addba36045a958f8d50e48a4b062db389fe01253
ThePurpleAthlete <jorisezta@gmail.com>
2024-11-05 14:34:49 +0100

    Update Game.py

--- Game.py
+++ Game.py
@@ -13,39 +13,85 @@ acht = pygame.image.load(r"achtergrond.png").convert()
 clock = pygame.time.Clock()
 running = True
 dt = clock.tick(60) / 1000
-a = 1
+a = 2
+
 class entity:
     def __init__(character, name, size, hp, damage, speed): 
         character.name = name
         character.size = size
         character.hp = hp
         character.damage = damage
-        character.speed = speed    
+        character.speed = speed
+
 
 class enemy(entity):
     def __init__(character, name, size, hp, damage, speed):
         super().__init__(name, size, hp, damage, speed)
 
+
 class player(entity):
     def __init__(character, name, size, hp, damage, speed):
         super().__init__(name, size, hp, damage, speed)
 
+
+
 def draw(enemy):
-        pygame.draw.circle(screen, "red", enemy_pos, enemy.size)
+    pygame.draw.circle(screen, "red", enemy_pos1, enemy.size)
+    pygame.draw.circle(screen, "red", enemy_pos2, enemy.size)
+
 
 def spawn_timer():
     global spawn_tijd
     spawn_tijd = 10
     while spawn_tijd >= 0:
         for x in range(spawn_tijd):
-            spawn_tijd -=1
+            spawn_tijd -= 1
             time.sleep(1)
-spawn_timer_thread = threading.Thread(target = spawn_timer, daemon=True)
+
+def melee_enemy_hitbox(enemy_pos1, enemy_pos2):
+    if math.dist((enemy_pos1), (enemy_pos2)) < melee_enemy.size + melee_enemy.size:
+        if enemy_pos1.x < enemy_pos2.x:
+            enemy_pos1.x -= 0.5 * melee_enemy.speed
+        if enemy_pos1.y < enemy_pos2.y:
+            enemy_pos1.y -= 0.5 * melee_enemy.speed
+        if enemy_pos1.x > enemy_pos2.x:
+            enemy_pos1.x += 0.5 * melee_enemy.speed
+        if enemy_pos1.y > enemy_pos2.y:
+            enemy_pos1.y += 0.5 * melee_enemy.speed
+        if enemy_pos2.x < enemy_pos1.x:
+            enemy_pos2.x -= 0.5 * melee_enemy.speed
+        if enemy_pos2.y < enemy_pos1.y:
+            enemy_pos2.y -= 0.5 * melee_enemy.speed
+        if enemy_pos2.x > enemy_pos1.x:
+            enemy_pos2.x += 0.5 * melee_enemy.speed
+        if enemy_pos2.y > enemy_pos1.y:
+            enemy_pos2.y += 0.5 * melee_enemy.speed
+
+
+        if enemy_pos1.x < player_pos.x:
+            enemy_pos1.x -= 0.5 * melee_enemy.speed
+        if enemy_pos1.y < player_pos.y:
+            enemy_pos1.y -= 0.5 * melee_enemy.speed
+        if enemy_pos1.x > player_pos.x:
+            enemy_pos1.x += 0.5 * melee_enemy.speed
+        if enemy_pos1.y > player_pos.y:
+            enemy_pos1.y += 0.5 * melee_enemy.speed
+        if enemy_pos2.x < player_pos.x:
+            enemy_pos2.x -= 0.5 * melee_enemy.speed
+        if enemy_pos2.y < player_pos.y:
+            enemy_pos2.y -= 0.5 * melee_enemy.speed
+        if enemy_pos2.x > player_pos.x:
+            enemy_pos2.x += 0.5 * melee_enemy.speed
+        if enemy_pos2.y > player_pos.y:
+            enemy_pos2.y += 0.5 * melee_enemy.speed
+
+
+spawn_timer_thread = threading.Thread(target=spawn_timer, daemon=True)
 spawn_timer_thread.start()
 
 
-def melee_enemy_movement(enemy_pos): 
-    if math.dist((enemy_pos), (player_pos)) >= player.size + melee_enemy.size:  
+def melee_enemy_movement(enemy_pos):
+    if math.dist((enemy_pos), (player_pos)) >= player.size + melee_enemy.size:
         if enemy_pos.x < player_pos.x:
             enemy_pos.x += melee_enemy.speed
         if enemy_pos.y < player_pos.y:
@@ -54,19 +100,27 @@ def melee_enemy_movement(enemy_pos):
             enemy_pos.x -= melee_enemy.speed
         if enemy_pos.y > player_pos.y:
             enemy_pos.y -= melee_enemy.speed
+
         # if math.dist((enemy_pos), (player_pos)) < player.size + melee_enemy.size:
 
 
-def border(player_pos):  
-    if player_pos.x < 0: player_pos.x = 0
-    if player_pos.y < 0: player_pos.y = 0
-    if player_pos.x > 1280: player_pos.x = 1280
-    if player_pos.y > 720: player_pos.y = 720
+def border(player_pos):
+    if player_pos.x < 0:
+        player_pos.x = 0
+    if player_pos.y < 0:
+        player_pos.y = 0
+    if player_pos.x > 1280:
+        player_pos.x = 1280
+    if player_pos.y > 720:
+        player_pos.y = 720
+
 
 player = player("player", 25, 60, 10, 300 * dt)
 melee_enemy = enemy("melee_enemy", 25, 50, 10, 150 * dt)
 ranged_enemy = enemy("ranged_enemy", 15, 20, 15, 150 * dt)
 player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+
+sword_pos = pygame.Vector2(screen.get_width() / 2 + 30, screen.get_height() / 2 + 30)
 
 
 while running:
@@ -74,7 +128,7 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    screen.blit(acht, (0,0))
+    screen.blit(acht, (0, 0))
     green_dot = pygame.draw.circle(screen, "green", player_pos, player.size)
     keys = pygame.key.get_pressed()
     if keys[pygame.K_UP] or keys[pygame.K_w]:
@@ -85,17 +139,37 @@ while running:
         player_pos.x -= player.speed
     if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
         player_pos.x += player.speed
-    
-    
-    if a == 1:
-        enemy_pos = pygame.Vector2(random.randint(0, 1280), random.randint(0, 720))
-        a = a - 1
-        
-    draw(melee_enemy)
-    melee_enemy_movement(enemy_pos)
 
+    if a == 2:
+        enemy_pos1 = pygame.Vector2(random.randint(0, 1280), random.randint(0, 720))
+        a = a - 1
+    if a == 1:
+        enemy_pos2 = pygame.Vector2(random.randint(0, 1280), random.randint(0, 720))
+        a = a - 1
+
+    sword = pygame.draw.circle(screen, "purple", sword_pos, 15)
+    while math.dist((sword_pos), (player_pos)) > 50:
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_UP] or keys[pygame.K_w]:
+            sword_pos.y -= player.speed
+        if keys[pygame.K_DOWN] or keys[pygame.K_s]:
+            sword_pos.y += player.speed
+        if keys[pygame.K_LEFT] or keys[pygame.K_a]:
+            sword_pos.x -= player.speed
+        if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
+            sword_pos.x += player.speed
+
+
+
+    melee_enemy_hitbox(enemy_pos1, enemy_pos2)
+    draw(melee_enemy)
+    melee_enemy_movement(enemy_pos1)
+    melee_enemy_movement(enemy_pos2)
+
+    border(sword_pos)
     border(player_pos)
-    border(enemy_pos)
+    border(enemy_pos1)
+    border(enemy_pos2)
 
     if spawn_tijd <= 0:
         print('wow')
